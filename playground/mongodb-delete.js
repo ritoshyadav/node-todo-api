3c010f9c76379044446387458408ddb3fba37259
paritosh535 <paritoshy535@gmail.com>
2018-11-08 23:04:16 +0545

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,28 @@
+const {MongoClient, ObjectID} = require('mongodb'); 
+
+MongoClient.connect('mongodb://localhost:27017', (err, cleint) => {
+    var db=cleint.db('TodoApps');
+    if(err) {
+        return console.log('Unable to connect mongoDB Sever');
+    }
+    console.log('Connected to MongoDB server');
+    
+// delete many
+db.collection('Todos').deleteMany({text: 'Something to do'}).then((result) =>{
+    console.log(result);
+})  ;
+
+// delete one
+db.collection('Todos').deleteOne({text: 'Something to do'}).then((result) =>{
+    console.log(result);
+})  ;
+
+// findOneAnddelete
+db.collection('Todos').findOneAndDelete({text: 'Something to do'}).then((result) =>{
+    console.log(result);
+})  ;
+
+
+
+
+});
