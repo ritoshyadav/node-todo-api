1b9cde2dac089824333efe6b11902d89f7a5f970
paritosh535 <paritoshy535@gmail.com>
2018-11-09 12:33:00 +0545

    Add Get Api

--- server/server.js
+++ server/server.js
@@ -16,10 +16,17 @@ var todocreate= new Todo({
 todocreate.save().then((doc) => {
     res.send(doc);
 },(e) =>{
-    res.send(e);
+    res.status(400).send(e);
 });
 });
 
+app.get('/todos',(req, res) =>{
+    Todo.find().then((todos) =>{
+        res.send({todos});
+    },(e) =>{
+        res.Status(400).send(e);
+    });
+});
 
 app.listen(3000,() =>{
     console.log('Started on port 3000');
